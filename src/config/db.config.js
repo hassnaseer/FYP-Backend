fb09e68b90601db50872ac0819a373d1e0dea55f
malik hassam <muhammad.hassam@argonteq.com>
2022-05-23 11:22:00 +0500

    reset Api

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -4,9 +4,9 @@ require('dotenv').config();
 const {DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME} = process.env;
 
 const sequelize = new Sequelize(DB_NAME,DB_USER,DB_PASSWORD,{
-    dialect: 'postgres',
     host:DB_HOST,
     port:DB_PORT,
+    dialect: 'postgres',
     ssl: true,
 })
 
