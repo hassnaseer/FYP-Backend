0f932dd0afb4e227fdb6103f935e42f372c6dc3b
malik hassam <muhammad.hassam@argonteq.com>
2022-06-16 13:15:51 +0500

    nodemailer work has been done

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -8,8 +8,12 @@ exports.sendEmail = (email, subject, password) => {
   let transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
+      type: 'OAuth2',
       user: process.env.MAIL_USER,
-      pass: process.env.MAIL_PASS
+      pass: process.env.MAIL_PASS,
+      clientId: process.env.OAUTH_CLIENTID,
+      clientSecret: process.env.OAUTH_CLIENT_SECRET,
+      refreshToken: process.env.OAUTH_REFRESH_TOKEN
     }
   });
 
@@ -34,10 +38,15 @@ exports.sendForgotPasswordEmail = (email, subject, code) => {
   let transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
+      type: 'OAuth2',
       user: process.env.MAIL_USER,
-      pass: process.env.MAIL_PASS
+      pass: process.env.MAIL_PASS,
+      clientId: process.env.OAUTH_CLIENTID,
+      clientSecret: process.env.OAUTH_CLIENT_SECRET,
+      refreshToken: process.env.OAUTH_REFRESH_TOKEN
     }
   });
+  
   let mailOptions = {
     from: process.env.MAIL_USER,
     to: email,
