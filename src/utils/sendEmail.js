2d97d64981ca9416904ededfc6085356d85a1cd9
malik hassam <muhammad.hassam@argonteq.com>
2022-09-05 14:55:04 +0500

    resolve email issue

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -3,63 +3,47 @@ const smtpTransport = require('nodemailer-smtp-transport');
 const { prepareOtpTemplate } = require('./../email_templates/otp_template');
 const { prepareForgotPasswordTemplate } = require('./../email_templates/forgot_password');
 
-exports.sendEmail = (email, subject, password) => {
-
+exports.sendEmail = async (email, subject, password) => {
   let transporter = nodemailer.createTransport({
-    service: 'gmail',
+    name: process.env.MAIL_HOST,
+    host: process.env.MAIL_HOST,
+    secure: false,
     auth: {
-      type: 'OAuth2',
       user: process.env.MAIL_USER,
-      pass: process.env.MAIL_PASS,
-      clientId: process.env.OAUTH_CLIENTID,
-      clientSecret: process.env.OAUTH_CLIENT_SECRET,
-      refreshToken: process.env.OAUTH_REFRESH_TOKEN
+      pass: process.env.MAIL_PASS
     }
   });
   let mailOptions = {
     from: process.env.MAIL_USER,
     to: email,
     subject: subject,
-    // html: prepareOtpTemplate(password)
+    html: prepareOtpTemplate(password)
   };
-  console.log(mailOptions, "=================")
-
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log('Email sent: ' + info.response);
-    }
-  });
+  const response = await transporter.sendMail(mailOptions)
+  if (response.rejected.length > 0) {
+    return new APIError(MESSAGES.EMAIL_UNSUCCESSFUL, status.BAD_REQUEST)
+  }
 
 }
-exports.sendForgotPasswordEmail = (email, subject, code) => {
-
+exports.sendForgotPasswordEmail = async (email, subject, code) => {
   let transporter = nodemailer.createTransport({
-    service: 'gmail',
+    name: process.env.MAIL_HOST,
+    host: process.env.MAIL_HOST,
+    secure: false,
     auth: {
-      type: 'OAuth2',
       user: process.env.MAIL_USER,
-      pass: process.env.MAIL_PASS,
-      clientId: process.env.OAUTH_CLIENTID,
-      clientSecret: process.env.OAUTH_CLIENT_SECRET,
-      refreshToken: process.env.OAUTH_REFRESH_TOKEN
+      pass: process.env.MAIL_PASS
     }
   });
-  
   let mailOptions = {
     from: process.env.MAIL_USER,
     to: email,
     subject: subject,
     html: prepareForgotPasswordTemplate(code)
   };
-
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log('Email sent: ' + info.response);
-    }
-  });
+  const response = await transporter.sendMail(mailOptions)
+  if (response.rejected.length > 0) {
+    return new APIError(MESSAGES.EMAIL_UNSUCCESSFUL, status.BAD_REQUEST)
+  }
 
 }
